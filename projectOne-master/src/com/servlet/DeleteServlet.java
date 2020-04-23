8e1a6790ec8df3f1797d70671047761203b89efd
Joseph-Alfred <44287533+Joseph-Alfred@users.noreply.github.com>
2020-04-23 10:34:50 +0530

    Add files via upload

---
+++ projectOne-master/src/com/servlet/DeleteServlet.java
@@ -0,0 +1,58 @@
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.UserDao;
+import com.dao.UserDaoImpl;
+
+/**
+ * Servlet implementation class DeleteServlet
+ */
+@WebServlet("/DeleteServlet")
+public class DeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DeleteServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		int userId = Integer.parseInt(request.getParameter("employeeId"));
+		UserDao userDao=new UserDaoImpl();
+		String str=null;
+		boolean deleteUserByid = userDao.deleteUserByid(userId);
+		if(deleteUserByid) {
+			str="User Deleted Sucessfully";
+            request.setAttribute("message",str);
+            request.getRequestDispatcher("delete-user-success.jsp").forward(request, response);
+		}
+
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
